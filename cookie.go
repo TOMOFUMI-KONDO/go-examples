3af8d56cbf95f98c9b59e3682263facdf5eda484
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-01-20 21:47:59 +0900

    added files

---
+++ cookie.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handlerWithCookie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Set-Cookie", "VISIT=TRUE")
+	if _, ok := r.Header["Cookie"]; ok {
+		fmt.Fprintf(w, "<html><body>2回目以降</body></html>")
+	} else {
+		fmt.Fprintf(w, "<html><body>初めてのリクエスト</body></html>")
+	}
+}
+
+func main() {
+	var httpServer http.Server
+	http.HandleFunc("/", handlerWithCookie)
+	log.Println("start http listening :18888")
+	httpServer.Addr = ":18888"
+	log.Println(httpServer.ListenAndServe())
+}
