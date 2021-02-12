5d9263749bcd5be231e875fdddb4b3218f14350d
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-02-12 21:22:31 +0900

    chunk response

---
+++ chunk/chunkServer.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func handlerChunkResponse(w http.ResponseWriter, r *http.Request) {
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		panic("expected http.ResponseWriter to be an http.Flusher")
+	}
+
+	for i := 1; i <= 10; i++ {
+		fmt.Fprintf(w, "Chunk #%d\n", i)
+		flusher.Flush()
+		time.Sleep(500 * time.Millisecond)
+	}
+	flusher.Flush()
+}
+
+func main() {
+	var httpServer http.Server
+	http.HandleFunc("/chunked", handlerChunkResponse)
+	log.Println("start http listening :18888")
+	httpServer.Addr = ":18888"
+	log.Println(httpServer.ListenAndServe())
+}
---
+++ chunk/superChunkClient.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+func main() {
+	// open TCP socket
+	dialer := &net.Dialer{
+		Timeout:   30 * time.Second,
+		KeepAlive: 30 * time.Second,
+	}
+	conn, err := dialer.Dial("tcp", "localhost:18888")
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	// send request
+	request, err := http.NewRequest("GET", "http://localhost:18888/chunked", nil)
+	err = request.Write(conn)
+	if err != nil {
+		panic(err)
+	}
+
+	// read
+	reader := bufio.NewReader(conn)
+
+	// read header
+	resp, err := http.ReadResponse(reader, request)
+	if err != nil {
+		panic(err)
+	}
+
+	if resp.TransferEncoding[0] != "chunked" {
+		panic("wrong transfer encoding")
+	}
+
+	for {
+		// get size
+		sizeStr, err := reader.ReadBytes('\n')
+		if err == io.EOF {
+			break
+		}
+		// parse hex size, if zero then close
+		size, err := strconv.ParseInt(string(sizeStr[:len(sizeStr)-2]), 16, 64)
+		if size == 0 {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+
+		// allocate buffer for size, read
+		line := make([]byte, int(size))
+		reader.Read(line)
+		reader.Discard(2)
+		log.Println(" ", string(line))
+	}
+}
