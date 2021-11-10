8c8fb1c411813e335fa0ea52738780e4779e5da8
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-11-10 18:56:36 +0900

    impl simple quic request by quic-go example
    see https://github.com/lucas-clemente/quic-go/blob/master/example/echo/echo.go

---
+++ quic/client.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"io"
+
+	"github.com/lucas-clemente/quic-go"
+)
+
+func main() {
+	if err := client(); err != nil {
+		panic(err)
+	}
+}
+
+func client() error {
+	tlsConf := &tls.Config{
+		InsecureSkipVerify: true,
+		NextProtos:         []string{"quic-echo-example"},
+	}
+	session, err := quic.DialAddr(addr, tlsConf, nil)
+	if err != nil {
+		return err
+	}
+
+	stream, err := session.OpenStreamSync(context.Background())
+	if err != nil {
+		return err
+	}
+
+	message := "hello"
+	fmt.Printf("Client: Sending '%s'\n", message)
+	_, err = stream.Write([]byte(message))
+	if err != nil {
+		return err
+	}
+
+	buf := make([]byte, len(message))
+	_, err = io.ReadFull(stream, buf)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Client: Got '%s'\n", buf)
+
+	return nil
+}
---
+++ quic/var.go
@@ -0,0 +1,5 @@
+package main
+
+var (
+	addr = "localhost:4430"
+)
