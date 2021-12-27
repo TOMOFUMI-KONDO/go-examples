faab4333cc5506ac6bb297c7d12a17f37cef2a7b
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-12-27 11:53:18 +0900

    feat: create http2 serverpush

---
+++ http2/serverpush.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+var (
+	port  = 18443
+	image []byte
+)
+
+func init() {
+	var err error
+	image, err = ioutil.ReadFile("./image.png")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", handleHtml)
+	http.HandleFunc("/image", handleImage)
+
+	fmt.Printf("start http listening :%d\n", port)
+	fmt.Println(http.ListenAndServeTLS(fmt.Sprintf(":%d", port), "../tls/server.crt", "../tls/server.key", nil))
+}
+
+func handleHtml(w http.ResponseWriter, r *http.Request) {
+	pusher, ok := w.(http.Pusher)
+	if ok {
+		pusher.Push("/image", nil)
+	}
+
+	w.Header().Add("Content-Type", "text/html")
+	fmt.Fprintf(w, `<html><body><img src="/image" /></body></html>`)
+}
+
+func handleImage(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "image/png")
+	w.Write(image)
+}
