70f570df21906abd94ea8ecedbb1405c08c8b5af
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2022-02-11 21:56:04 +0900

    fix: ctfbook 25 answer

--- ctfbook/25-7-2/main.go
+++ ctfbook/25-7-2/main.go
@@ -5,7 +5,8 @@ func main() {
 }
 
 func f(a int32) int32 {
-	if a&1 == 1 {
+	// if a&1 == 0
+	if a%2 == 0 {
 		return 1
 	} else {
 		return 0
--- ctfbook/25-7-3/main.go
+++ ctfbook/25-7-3/main.go
@@ -6,11 +6,9 @@ func main() {
 
 func f(a int32) int32 {
 	var b int32 = 0
-	var c int32 = 1
 
-	for c <= a {
-		b += c
-		c += 1
+	for i := 1; int32(i) <= a; i++ {
+		b += int32(i)
 	}
 	return b
 }
